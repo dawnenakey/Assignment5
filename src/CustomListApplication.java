8555c82f4e07764c2362ad9fed80ae54fae0db1e
dawnenakey <91962932+dawnenakey@users.noreply.github.com>
2022-07-26 13:35:02 -0500

    first commit

---
+++ src/CustomListApplication.java
@@ -0,0 +1,45 @@
+package com.coderscampus.arraylist;
+
+public class CustomListApplication {
+
+	public static void main(String[] args) {
+		
+		CustomList<String> myCustomList = new CustomArrayList<>();
+		
+		myCustomList.add(1);
+		myCustomList.add(2);
+		myCustomList.add(3);
+		myCustomList.add(4);
+		myCustomList.add(5);
+		myCustomList.add(6);
+		myCustomList.add(7);
+		myCustomList.add(8);
+		myCustomList.add(9);
+		myCustomList.add(10);
+		myCustomList.add(11);
+		myCustomList.add(12);
+		myCustomList.add(13);
+		myCustomList.add(14);
+		myCustomList.add(15);
+		myCustomList.add(16);
+		myCustomList.add(17);
+		myCustomList.add(18);
+		myCustomList.add(19);
+		myCustomList.add(20);
+		myCustomList.add(21);
+
+			
+
+		
+		for (int i=0; i< items.getSize(); i++) {
+			System.out.println(items.get(i));
+			
+		}
+		
+		
+		
+		
+
+	}
+
+}
